e36fe6df7b606b14a6e0750ae37d2ff5b93b1fbc
pulciblight <kadynnik@mail.ru>
2025-05-05 19:58:28 +0300

    попробовала написать начало кода, но думаю, что структуру лучше реализовать иначе. еще нужно клавиатуры обсудить

--- clava.py
+++ clava.py
@@ -1,18 +1,21 @@
 from telebot import types
-from repliki import scenario
-keyboard1 = types.ReplyKeyboardMarkup(row_width=2)
+
+start_keyboard = types.ReplyKeyboardMarkup(row_width=2)
 button1 = types.KeyboardButton('Начать игру')
 button2 = types.KeyboardButton('Узнать легенду')
-keyboard1.add(button1, button2)
-def key_chooser(text):
-    keyboard = types.ReplyKeyboardMarkup(row_width=2)
-    if scenario[text] == 1:
-        continued = types.KeyboardButton('Далее')
-        keyboard.add(continued)
-    elif scenario[text] == 2:
-        button1 = types.KeyboardButton('Написать в чат, что у тебя не работает микрофон: Отлично! Преподаватель поверил тебе и спросил твоего одногруппника.')
-        button2 = types.KeyboardButton('Ответить на вопрос своими словами и попытаться заболтать преподавателя')
-        keyboard.add(button1, button2)
-    legend = types.KeyboardButton('Ресурсы')
-    keyboard.add(legend)
+start_keyboard.add(button1, button2)
+
+sex_choose = types.ReplyKeyboardMarkup(row_width=2)
+man = types.KeyboardButton('Девушка')
+woman = types.KeyboardButton('Парень')
+sex_choose.add(man, woman)
+
+basic = types.ReplyKeyboardMarkup(row_width=2)
+basic.add(types.KeyboardButton('Далее'))
+
+
+def key_chooser(num_of_choices):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for i in range(1, num_of_choices + 1):
+        keyboard.add(types.KeyboardButton(str(i)))
     return keyboard
--- game.py
+++ game.py
@@ -1,21 +1,20 @@
 import telebot
-from clava import keyboard1, key_chooser
-import repliki
+from requests import options
+
+from clava import basic, key_chooser, start_keyboard, sex_choose
+from scenario import scene
+
 API_TOKEN = '7842674848:AAHaZqKSI2gplCBCPo89O52YJXauRz3DuNU'
 bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-  bot.reply_to(message, 'Привет! Я бот.', reply_markup=keyboard1)
+  bot.reply_to(message, 'Привет! Я бот.', reply_markup=start_keyboard)
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: message.text in ['Начать игру', 'Узнать легенду'])
 def handle_message(message):
   if message.text == 'Начать игру':
-      bot.send_message(message.chat.id, repliki.intro , reply_markup=key_chooser(repliki.intro))
+      bot.send_message(message.chat.id, 'пжшки скажите какого вы пола' , reply_markup=sex_choose)
   elif message.text == 'Узнать легенду':
       bot.send_message(message.chat.id, 'Вы - студент-первокурсник. Будьте добры, выживите))')
-  elif message.text == 'Далее':
-      bot.send_message(message.chat.id, repliki.first , reply_markup=key_chooser(repliki.first))
-  elif message.text == 'Ресурсы':
-      bot.send_message(message.chat.id, repliki.resource, reply_markup=key_chooser(repliki.resource))
 
 bot.polling()
