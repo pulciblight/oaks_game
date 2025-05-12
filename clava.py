629954ed0fc3affada4d2e00c956511630013dee
pulciblight <kadynnik@mail.ru>
2025-05-12 23:39:01 +0300

    не поверите написала завтрак

--- clava.py
+++ clava.py
@@ -1,21 +1,7 @@
 from telebot import types
 
-start_keyboard = types.ReplyKeyboardMarkup(row_width=2)
-button1 = types.KeyboardButton('Начать игру')
-button2 = types.KeyboardButton('Узнать легенду')
-start_keyboard.add(button1, button2)
-
-sex_choose = types.ReplyKeyboardMarkup(row_width=2)
-man = types.KeyboardButton('Девушка')
-woman = types.KeyboardButton('Парень')
-sex_choose.add(man, woman)
-
-basic = types.ReplyKeyboardMarkup(row_width=2)
-basic.add(types.KeyboardButton('Далее'))
-
-
 def key_chooser(options):
-    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for option in options:
         keyboard.add(types.KeyboardButton(option))
     return keyboard
--- game.py
+++ game.py
@@ -1,20 +1,42 @@
 import telebot
-from requests import options
-
-from clava import basic, key_chooser, start_keyboard, sex_choose
-from scenario import scene
+from states import start_states, states_m, states_f
+from clava import key_chooser
 
 API_TOKEN = '7842674848:AAHaZqKSI2gplCBCPo89O52YJXauRz3DuNU'
 bot = telebot.TeleBot(API_TOKEN)
+
+
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-  bot.reply_to(message, 'Привет! Я бот.', reply_markup=start_keyboard)
+    user_id = message.from_user.id
+    bot.send_message(user_id, start_states['start']['text'],
+                    reply_markup=key_chooser(start_states['start']['options']))
+
 
 @bot.message_handler(func=lambda message: message.text in ['Начать игру', 'Узнать легенду'])
 def handle_message(message):
-  if message.text == 'Начать игру':
-      bot.send_message(message.chat.id, 'пжшки скажите какого вы пола' , reply_markup=sex_choose)
-  elif message.text == 'Узнать легенду':
-      bot.send_message(message.chat.id, 'Вы - студент-первокурсник. Будьте добры, выживите))')
+    user_id = message.from_user.id
+    text = message.text
+    bot.send_message(user_id, start_states[text]['text'],
+                     reply_markup=key_chooser(start_states[text]['options']))
 
+
+@bot.message_handler(func=lambda message: message.text in ['Женский', 'Мужской'])
+def sex_assignment(message):
+    global scenario
+    user_id = message.from_user.id
+    text = message.text
+    if text == 'Женский':
+        scenario = states_f.copy()
+    elif text == 'Мужской':
+        scenario = states_m.copy()
+    bot.send_message(user_id, scenario[text]['text'],
+                     reply_markup=key_chooser(scenario[text]['options']))
+
+@bot.message_handler(func=lambda message: message.text in scenario.keys())
+def finally_game(message):
+    user_id = message.from_user.id
+    text = message.text
+    bot.send_message(user_id, scenario[text]['text'],
+                    reply_markup=key_chooser(scenario[text]['options']))
 bot.polling()
