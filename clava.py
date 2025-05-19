66a694d79052427cb19e22827d2be84a77ed97f4
Pikhutkina <polinakhutkina@gmail.com>
2025-05-19 16:30:06 +0300

    Delete clava.py

--- clava.py
+++
@@ -1,7 +0,0 @@
-from telebot import types
-
-def key_chooser(options):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    for option in options:
-        keyboard.add(types.KeyboardButton(option))
-    return keyboard
